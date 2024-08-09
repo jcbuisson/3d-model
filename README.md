1edce563dcd4fd6c37ea2a70faac37522c6c7477
JC BUISSON <buisson@enseeiht.fr>
2024-08-09 07:06:20 +0200

    WIP

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Vue 3 + Vite
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+# Modèles 3D
 
-Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+vue-3d-loader : https://github.com/king2088/vue-3d-loader/tree/master/src/examples
+
+https://www.mixamo.com
+
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
+   
+   <!-- choix du modèle -->
    <select v-model="selected">
       <template v-for="model in models">
          <option :value="model">{{ model.name }}</option>
       </template>
    </select>
 
+   <!-- viewer -->
    <vue3dLoader
       height="500"
       :scale="selected.scale"
@@ -14,6 +17,7 @@
       @load="onLoad"
    ></vue3dLoader>
 
+   <!-- waiting spinner -->
    <div v-if="spinnerWaitingText" class="fixed top-12 left-0 m-4 p-4 bg-green-100 border-solid rounded z-50">
       <div>
          <template v-for="line in spinnerWaitingText">
@@ -28,7 +32,7 @@
 import { ref } from 'vue'
 
 const models = [
-   { filePath: "https://ftp.jcbuisson.dev/3d-models/helmet.fbx", name: "Casque", scale: {x: 0.8, y: 0.8, z: 0.8 }},
+   { filePath: "https://ftp.jcbuisson.dev/3d-models/van.glb", name: "Volkswagen Van (GLB)", scale: {x: 0.8, y: 0.8, z: 0.8 }},
    { filePath: "https://ftp.jcbuisson.dev/3d-models/arthology.fbx", name: "Arthologie (FBX)", scale: {x: 0.8, y: 0.8, z: 0.8 }},
    { filePath: "https://ftp.jcbuisson.dev/3d-models/muscular-insertions.fbx", name: "Insertions musculaires (FBX)", scale: {x: 0.8, y: 0.8, z: 0.8 }},
    { filePath: "https://ftp.jcbuisson.dev/3d-models/splanchnology.fbx", name: "Splanchnologie (FBX)", scale: {x: 0.8, y: 0.8, z: 0.8 }},
